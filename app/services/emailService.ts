645a88dd57c25ccf1cdaf4f32d61e55bf0984ae3
JrgCanales <jorgecanalesortega935@gmail.com>
2025-09-02 19:06:53 -0600

    test email port

--- app/services/emailService.ts
+++ app/services/emailService.ts
@@ -14,8 +14,8 @@ interface DatosVenta {
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    port: process.env.EMAIL_PORT ? parseInt(process.env.EMAIL_PORT) : 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
